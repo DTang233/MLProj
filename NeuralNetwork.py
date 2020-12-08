13616ab05aa3df4a1c099492744bb7cfc70eec39
weishig <47680583+weishig@users.noreply.github.com>
2020-12-07 23:45:26 -0500

    1

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -19,17 +19,22 @@ training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data) 
 
-x_train=createArray(50000,28*28,training_data,0) #50000 x 784
-y_train=createArray(50000,10,training_data,1)#50000 x 10
-x_test=createArray(10000,28*28,test_data,0)#10000 x 784
-y_test=np.zeros((10000,10))
+#Initialize data 
+x_train=createArray(50000,28*28,training_data,0) # shape(50000 x 784)
+y_train=createArray(50000,10,training_data,1)#shape(50000 x 10)
+x_test=createArray(10000,28*28,test_data,0)#shape(10000 x 784)
+y_test=np.zeros((10000,10))#shape(10000 x 10)
 for i in range(0,10000):
     y_test[i][test_data[i][1]]=1
 
+
 #Build the model
 model=Sequential()
-model.add(Dense(128, activation='sigmoid',input_dim=784))
-model.add(Dense(64, activation='sigmoid'))
+#layer 1 settings, such as neuron numbers, weight and bias initialization, activation function
+model.add(Dense(128, activation='sigmoid',kernel_initializer='random_normal',bias_initializer='zeros',input_dim=784))
+#layer 2 settings
+model.add(Dense(64, kernel_initializer='random_normal',bias_initializer='zeros',activation='sigmoid'))
+#output layer settings
 model.add(Dense(10, activation='softmax'))
 print("model created successful")
 
@@ -41,14 +46,16 @@ model.compile(
 )
 print("compile model successful")
 
+#train the model with 500000 data
 model.fit(
     x_train,
     y_train,
-    epochs=5,
-    batch_size=100
+    epochs=5, # will run 500000 data for 5 times
+    batch_size=32 
 )
-
 print("evaluate test")
+
+#evaluate the model on 10000 data
 model.evaluate(
     x_test,
     y_test  
