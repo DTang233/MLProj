16372ee8d2498493a3496acc2623ea771b43eade
maxtang233 <tangd94@berkeley.edu>
2020-12-07 22:11:49 -0500

    added analysis

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -11,9 +11,9 @@ X2, y2 = read_dataset ("project3_dataset2.txt")
 clf1 = LogisticRegression(random_state=0).fit(X1, y1)
 clf2 = LogisticRegression(random_state=0).fit(X2, y2)
 #predict the first 10 data?
-clf1.predict_proba(X1[:10, :])
+print(clf1.predict_proba(X1))
 clf2.predict_proba(X2[:10, :])
-#Return the mean accuracy on the given test data and labels.
-print(clf1.score(X1, y1))
-print(clf2.score(X2, y2))
+# #Return the mean accuracy on the given test data and labels.
+# print(clf1.score(X1, y1))
+# print(clf2.score(X2, y2))
 
--- run.py
+++ run.py
@@ -6,6 +6,10 @@ from sklearn.metrics import plot_precision_recall_curve
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import auc
+from sklearn.metrics import roc_curve
 from sklearn.svm import SVC
 from sklearn import tree
 
@@ -21,7 +25,8 @@ def ten_fold_cross_validation(X, y, algo, params):
 #Accuracy, Precision, Recall, F-1 measure, and AUC (area under
 #the curve)
 	
-	'''Here we divided X into 10 sets, 9/10 of the data are 
+	"""
+	Here we divided X into 10 sets, 9/10 of the data are 
 	using as training data each time, and the remaining 1/10 
 	are using as test data. 
 
@@ -30,8 +35,7 @@ def ten_fold_cross_validation(X, y, algo, params):
 
 	- X2 is a (462, 9) numpy array, 
 	- y2 is a (462, ) array
-
-	'''
+	"""
 
 	#Decide which classifier to use
 	if algo == 'Logistic Regression':
@@ -44,25 +48,66 @@ def ten_fold_cross_validation(X, y, algo, params):
 	if algo == 'Decision Tree':
 		clf = tree.DecisionTreeClassifier()
 
-
-	kf = KFold(n_splits=10)
-	i = 0
+	cv_nums = 10
+	kf = KFold(n_splits=cv_nums)
+	i = 1
+	training_accuracys = []
+	test_accuracys = []
+	AUCs = []
 	for train_index, test_index in kf.split(X):
 	# 	print("TRAIN:", train_index, "TEST:", test_index)
 		X_train = X[train_index]
 		y_train = y[train_index]
 		X_test = X[test_index]
 		y_test = y[test_index]
-		
 		clf.fit(X_train, y_train)
+
+		y_pred_train = clf.predict(X_train)
+		y_pred_test = clf.predict(X_test)
+		
+		#precision, recall and AUC for the test data
 		disp = plot_precision_recall_curve(clf, X_test, y_test)
-		disp.ax_.set_title('2-class Precision-Recall curve: ')
+		y_scores = clf.predict_proba(X_test)
+		precision, recall, thresholds = roc_curve(y_test, y_pred_test)
+
+		#f1 score of the train data
+		f1_score_train = f1_score(y_train, y_pred_train, average='micro')
+		#f1 score of the test data
+		f1_score_test = f1_score(y_test, y_pred_test, average='micro')
+		
+		AUCs.append(auc(recall, precision))
+
+
+		#get accuracy for training data
+		training_accuracys.append([accuracy_score(y_train, y_pred_train),f1_score_train])
+		#get accuracy for tests data
+		test_accuracys.append([accuracy_score(y_test, y_pred_test),f1_score_test])
+
+		disp.ax_.set_title('Iteration'+str(i) +' 2-class Precision-Recall curve: ')
+		
+
 		plt.savefig(algo+ str(i) +'.png')
 		i+=1
+	
+	for j in range(cv_nums):
+		print('------------Iteration '+str(j+1)+'-------------')
+		print('Training accuracy: ')
+		print(training_accuracys[j][0])
+		print('Training F-1 score: ')
+		print(training_accuracys[j][1])
+		print('Testing accuracy: ')
+		print(test_accuracys[j][0])
+		print('Testing F-1 score: ')
+		print(test_accuracys[j][1])
+		print('Testing AUC: ')
+		print(AUCs[j])
+
+
 
 
 if __name__ == '__main__':
-	ten_fold_cross_validation(X1, y1, 'SVM', None)
+	ten_fold_cross_validation(X1, y1, 'Logistic Regression', None)
+	# ten_fold_cross_validation(X1, y1, 'SVM', None)
 	# ten_fold_cross_validation(X1, y1, 'Knn', 3)
 
 
